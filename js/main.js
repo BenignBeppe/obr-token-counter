b4c461b3aec38c42b804644a74fb5d62b3bc49a7
Sebastian Berlin <berlin.sebastian@gmail.com>
2023-07-31 18:10:43 +0200

    Multiple counters
    Tokens can have multiple counters added to and removed from
    them. Colour for visualisation can be set to distinguish them.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
 import OBR, { buildText } from "@owlbear-rodeo/sdk";
-import { getPluginId } from "./util.js";
-import { updateVisualisation, addVisualistaion, removeVisualisation, getVisualisation } from "./visualisation.js";
+import { getPluginId, log } from "./util.js";
+import {
+    setVisualisationValue,
+    setVisualisationColour,
+    addVisualisation,
+    removeVisualisation,
+    getVisualisation,
+    hideVisualisation
+} from "./visualisation.js";
+
+const HIDDEN = "HIDDEN";
+const NUMBER = "NUMBER";
+
+async function addCounter() {
+    let token = await getSelectedToken();
+    if(!token) {
+        return;
+    }
+
+    await OBR.scene.items.updateItems([token], (items) => {
+        for(let item of items) {
+            if(!Object.hasOwn(item.metadata, getPluginId("counters"))) {
+                item.metadata[getPluginId("counters")] = [];
+            }
+            item.metadata[getPluginId("counters")].push({
+                value: 0,
+                showAs: HIDDEN,
+                colour: "#ffffff"
+            });
+        }
+    });
+
+    // We need to get the token again here to get the updated state.
+    let updatedToken = await getToken(token.id);
+    updateCounters(updatedToken);
+    log(`Added counter to token ${token.id} ("${token.name})"`);
+}
+
+async function getSelectedToken() {
+    let selectedIds = await OBR.player.getSelection();
+    let token = await getToken(selectedIds);
+    return token;
+}
 
 /**
  * Get the first token from item ids
@@ -22,70 +63,122 @@ async function getToken(itemIds) {
     return tokens[0];
 }
 
-async function getSelectedToken() {
-    let selectedIds = await OBR.player.getSelection();
-    let token = await getToken(selectedIds);
-    return token;
+async function getItem(itemId) {
+    let items = await OBR.scene.items.getItems([itemId]);
+    return items[0];
 }
 
-function updateCounter(token) {
-    valueField.value = token.metadata[getPluginId("value")] || 0;
+function updateCounters(token) {
+    let counters = token.metadata[getPluginId("counters")];
+    let elements = [];
+    for(let [index, counter] of counters.entries()) {
+        let template = document.querySelector("#templates .counter");
+        let element = template.cloneNode(true);
+        let metadata = token.metadata[getPluginId("counters")][index];
+
+        let valueInput = element.querySelector(".value");
+        valueInput.value = metadata.value;
+        valueInput.addEventListener(
+            "change",
+            () => updateValue(token.id, index, valueInput)
+        );
+
+        let hideButton = element.querySelector(".hide");
+        if(metadata.showAs === HIDDEN) {
+            hideButton.classList.add("selected");
+        }
+        hideButton.addEventListener(
+            "click",
+            () => {
+                updateShowAs(token.id, index, HIDDEN);
+                hideButton.classList.add("selected");
+                showNumberButton.classList.remove("selected");
+            }
+        );
+        let showNumberButton = element.querySelector(".show-number");
+        if(metadata.showAs === NUMBER) {
+            showNumberButton.classList.add("selected");
+        }
+        showNumberButton.addEventListener(
+            "click",
+            () => {
+                updateShowAs(token.id, index, NUMBER);
+                hideButton.classList.remove("selected");
+                showNumberButton.classList.add("selected");
+            }
+        );
+
+        let colourInput = element.querySelector(".colour");
+        colourInput.value = metadata.colour;
+        colourInput.addEventListener(
+            "change",
+            () => updateColour(token.id, index, colourInput)
+        );
+
+        let removeButton = element.querySelector(".remove");
+        removeButton.addEventListener(
+            "click",
+            () => removeCounter(token.id, index)
+        );
+
+        elements.push(element);
+    }
+    let counterList = document.querySelector("#counters");
+    counterList.replaceChildren(...elements);
 }
 
-async function updateShowButtons(token) {
-    let visualisation = await getVisualisation(token);
-    if(visualisation.length) {
-        hide.classList.remove("selected");
-        showNumberButton.classList.add("selected");
-    } else {
-        hide.classList.add("selected");
-        showNumberButton.classList.remove("selected");
+async function updateValue(tokenId, counterIndex, valueInput) {
+    let token = await getItem(tokenId);
+    let value = Number(valueInput.value);
+    OBR.scene.items.updateItems([token], (items) => {
+        for(let item of items) {
+            item.metadata[getPluginId("counters")][counterIndex].value = value;
+        }
+    });
+    setVisualisationValue(token, counterIndex, value);
+}
+
+async function updateShowAs(tokenId, counterIndex, showAs) {
+    let token = await getItem(tokenId);
+    OBR.scene.items.updateItems([token], (items) => {
+        for(let item of items) {
+            item.metadata[getPluginId("counters")][counterIndex].showAs = showAs;
+        }
+    });
+    if(showAs === HIDDEN) {
+        hideVisualisation(token, counterIndex);
+    } else if(showAs === NUMBER) {
+        addVisualisation(token, counterIndex);
     }
 }
 
-let valueField = document.querySelector("#value");
-valueField.addEventListener("change", async () => {
-    let token = await getSelectedToken();
-    if(!token) {
-        return;
-    }
+async function updateColour(tokenId, counterIndex, colourInput) {
+    let token = await getItem(tokenId);
+    let colour = colourInput.value;
+    OBR.scene.items.updateItems([token], (items) => {
+        for(let item of items) {
+            item.metadata[getPluginId("counters")][counterIndex].colour = colour;
+        }
+    });
+    setVisualisationColour(token, counterIndex, colour);
+}
 
-    let value = valueField.value;
+async function removeCounter(tokenId, counterIndex) {
+    let token = await getItem(tokenId);
     await OBR.scene.items.updateItems([token], (items) => {
         for(let item of items) {
-            item.metadata[getPluginId("value")] = value;
+            item.metadata[getPluginId("counters")].splice(counterIndex, 1);
         }
     });
-    updateVisualisation(token);
-});
 
-let hideButton = document.querySelector("#hide");
-hideButton.addEventListener("click", async () => {
-    let token = await getSelectedToken();
-    if(!token) {
-        return;
-    }
-
-    removeVisualisation(token);
-});
-let showNumberButton = document.querySelector("#show-number");
-showNumberButton.addEventListener("click", async () => {
-    let token = await getSelectedToken();
-    if(!token) {
-        return;
-    }
-
-    addVisualistaion(token);
-});
-for(let button of [hideButton, showNumberButton]) {
-    button.addEventListener("click", () => {
-        let selected = document.querySelector(".selected");
-        if(selected) {
-            selected.classList.remove("selected");
-        }
-        button.classList.add("selected");
-    });
+    // We need to get the token again here to get the updated state.
+    let updatedToken = await getItem(tokenId);
+    updateCounters(updatedToken);
+    removeVisualisation(updatedToken, counterIndex);
+    log(`Removed counter with index ${counterIndex} from token ${token.id} ("${token.name})"`);
 }
+
+document.querySelector("#add-counter").addEventListener("click", addCounter);
 
 OBR.onReady(() => {
     OBR.player.onChange(async (player) => {
@@ -95,7 +188,6 @@ OBR.onReady(() => {
             return;
         }
 
-        updateCounter(token);
-        updateShowButtons(token);
+        updateCounters(token);
     });
 });
--- js/visualisation.js
+++ js/visualisation.js
@@ -1,73 +1,136 @@
-export { updateVisualisation, addVisualistaion, removeVisualisation, getVisualisation };
+export {
+    setVisualisationValue,
+    setVisualisationColour,
+    addVisualisation,
+    removeVisualisation,
+    getVisualisation,
+    hideVisualisation
+};
 
 import OBR, { buildText } from "@owlbear-rodeo/sdk";
 import { getPluginId } from "./util.js";
 
 const VISUALISATION = "VISUALISATION";
+const VISUALISATION_HEIGHT = 50;
 
-async function updateVisualisation(token) {
-    let visualisation = await getVisualisation(token);
+async function setVisualisationValue(token, counterIndex, value) {
+    let visualisation = await getVisualisation(token, counterIndex);
     if(!visualisation.length) {
         return;
     }
 
-    // We need to get the up to date value for the token. The one in
-    // the parameter may be anold one.
-    token = await OBR.scene.items.getItems([token.id]);
-    let value = token[0].metadata[getPluginId("value")];
     OBR.scene.items.updateItems(visualisation, (items) => {
         for (let item of items) {
-            item.text.plainText = value;
+            item.text.plainText = String(value);
         }
     });
 }
 
-async function getVisualisation(token) {
+async function setVisualisationColour(token, counterIndex, colour) {
+    let visualisation = await getVisualisation(token, counterIndex);
+    if(!visualisation.length) {
+        return;
+    }
+
+    OBR.scene.items.updateItems(visualisation, (items) => {
+        for (let item of items) {
+            item.text.style.fillColor = colour;
+        }
+    });
+}
+
+async function getVisualisation(token, counterIndex) {
     let visualisation = await OBR.scene.items.getItems(
-        i => i.attachedTo === token.id &&
-            i.metadata[getPluginId("role")] === VISUALISATION
+        i => i.attachedTo === token.id
+            && i.metadata[getPluginId("role")] === VISUALISATION
+            && i.metadata[getPluginId("counterIndex")] === counterIndex
     );
     return visualisation;
 }
 
-async function createVisualisation(token) {
-    let gridDpi = await OBR.scene.grid.getDpi();
-    // TODO: Make position work with offset that isn't half of width and height.
-    let imageWidth = (token.image.width / token.grid.dpi) * gridDpi;
-    let x = token.position.x - (imageWidth / 2) * token.scale.x;
-    let imageHeight = (token.image.height / token.grid.dpi) * gridDpi;
-    let y = token.position.y - (imageHeight / 2) * token.scale.y;
-    let position = {x: x, y: y};
+async function getVisualisations(token, startIndex) {
+    startIndex = startIndex || 0;
+    let visualisations = await OBR.scene.items.getItems(
+        i => i.attachedTo === token.id
+            && i.metadata[getPluginId("role")] === VISUALISATION
+            && i.metadata[getPluginId("counterIndex")] >= startIndex
+    );
+    return visualisations;
+}
+
+async function createVisualisation(token, counterIndex) {
+    let metadata = token.metadata[getPluginId("counters")][counterIndex];
     let item = buildText()
         .textType("PLAIN")
-        .position(position)
-        .fontSize(40)
+        .plainText(String(metadata.value))
+        .fontSize(VISUALISATION_HEIGHT)
         .fontWeight(700)
+        .fillColor(metadata.colour)
         .strokeColor("black")
         .strokeWidth(2)
         .attachedTo(token.id)
         .locked(true)
-        .metadata({[getPluginId("role")]: VISUALISATION})
+        .metadata({
+            [getPluginId("role")]: VISUALISATION,
+            [getPluginId("counterIndex")]: counterIndex
+        })
         .build();
     await OBR.scene.items.addItems([item]);
 }
 
-async function addVisualistaion(token) {
-    let visualisation = await getVisualisation(token);
+async function rearangeVisualisations(token) {
+    let gridDpi = await OBR.scene.grid.getDpi();
+    // TODO: Make position work with offset that isn't half of width
+    // and height.
+    let imageWidth = (token.image.width / token.grid.dpi) * gridDpi;
+    let x = token.position.x - (imageWidth / 2) * token.scale.x;
+    let imageHeight = (token.image.height / token.grid.dpi) * gridDpi;
+    let y = token.position.y - (imageHeight / 2) * token.scale.y;
+
+    let visualisations = await getVisualisations(token);
+    visualisations.sort((a, b) => a.metadata[getPluginId("counterIndex")] - b.metadata[getPluginId("counterIndex")]);
+    let index = 0;
+    OBR.scene.items.updateItems(
+        visualisations,
+        (items) => {
+            for(let item of items) {
+                item.position.x = x;
+                item.position.y = y + VISUALISATION_HEIGHT * index;
+                index ++;
+            }
+        }
+    );
+}
+
+async function addVisualisation(token, counterIndex) {
+    let visualisation = await getVisualisation(token, counterIndex);
     if(visualisation.length) {
         return;
     }
 
-    await createVisualisation(token);
-    updateVisualisation(token);
+    await createVisualisation(token, counterIndex);
+    rearangeVisualisations(token);
 }
 
-async function removeVisualisation(token) {
-    let visualisation = await getVisualisation(token);
+async function hideVisualisation(token, counterIndex) {
+    let visualisation = await getVisualisation(token, counterIndex);
     if(!visualisation.length) {
         return;
     }
 
     let ids = visualisation.map(i => i.id);
-    OBR.scene.items.deleteItems(ids);
+    await OBR.scene.items.deleteItems(ids);
+    rearangeVisualisations(token);
+}
+
+async function removeVisualisation(token, counterIndex) {
+    hideVisualisation(token, counterIndex);
+
+    // Subtract one from the index of all visualisations after the one
+    // deleted to keep them in sync.
+    let visualisationsAfter = await getVisualisations(token, counterIndex + 1);
+    OBR.scene.items.updateItems(
+        visualisationsAfter,
+        items => items.forEach(i => i.metadata[getPluginId("counterIndex")] -= 1)
+    );
 }
